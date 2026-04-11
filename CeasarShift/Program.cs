72552fa0b158251f6653a7e2e8b32cf34a528085
jamel <jamel@JAMELS_LAPTOP>
2026-04-10 21:03:05 -0700

    Add project files.

---
+++ CeasarShift/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CeasarShift
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ CeasarShiftTest/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿namespace CeasarShiftTest
+{
+    public class Tests
+    {
+        CeasarShift.Cypher cc;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            cc = new CeasarShift.Cypher();
+        }
+
+        [Test]
+        public void shiftCorrectly()
+        {
+            var s = cc.shift("a", 5);
+            Assert.That(s, Is.EqualTo("f"));
+        }
+
+        [Test]
+
+        public void shiftBackCorrectly()
+        {
+            var s = cc.shift("d", 5);
+            Assert.That(s, Is.EqualTo("f"));
+        }
+
+        [Test]
+
+        public void shiftSentenceCorrectly()
+        {
+            var s = cc.shift("hey you your finally awake", 5);
+            Assert.That(s, Is.EqualTo("mjd dtz dtzw knsfqqd fbfpj"));
+        }
+
+        [Test]
+
+        public void shiftBackSentenceCorrectly()
+        {
+            var s = cc.shift("mjd dtz dtzw knsfqqd fbfpj", 5);
+            Assert.That(s, Is.EqualTo("hey you your finally awake"));
+
+        }
+    }
+}
